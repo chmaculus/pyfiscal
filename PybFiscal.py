d467d36f0b34a8227d2bc175f6e04b76e385da24
cam notebook <cam@notebookasus>
2024-04-07 21:31:50 -0300

    En la rama prueba_cam  Cambios a ser confirmados: 	modificados:     PybFiscal.py 	nuevos archivos: print.sh 	nuevos archivos: pyfiscalprinter/.gitignore 	nuevos archivos: pyfiscalprinter/0a.txt 	nuevos archivos: pyfiscalprinter/LICENSE 	nuevos archivos: pyfiscalprinter/README 	nuevos archivos: pyfiscalprinter/README.md 	nuevos archivos: pyfiscalprinter/__init__.py 	nuevos archivos: pyfiscalprinter/agente.py 	nuevos archivos: pyfiscalprinter/controlador.py 	nuevos archivos: pyfiscalprinter/ejemplo.php 	nuevos archivos: pyfiscalprinter/ejemplo.vbs 	nuevos archivos: pyfiscalprinter/epsonFiscal.py 	nuevos archivos: pyfiscalprinter/epsonFiscalDriver.py 	nuevos archivos: pyfiscalprinter/epsonFiscalDriver_service.py 	nuevos archivos: pyfiscalprinter/factura.json 	nuevos archivos: pyfiscalprinter/fiscal.ini 	nuevos archivos: pyfiscalprinter/fiscalGeneric.py 	nuevos archivos: pyfiscalprinter/hasarPrinter.py 	nuevos archivos: pyfiscalprinter/requirements.txt 	nuevos archivos: pyfiscalprinter/setup.py 	nuevos archivos: pyfiscalprinter/test.py

--- PybFiscal.py
+++ PybFiscal.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from pyfiscalprinter.hasarPrinter import HasarPrinter #from hasarPrinter import HasarPrinter
+# from pyfiscalprinter.hasarPrinter import HasarPrinter 
+#from hasarPrinter import HasarPrinter
+from pyfiscalprinter.epsonFiscal import EpsonPrinter 
+#from hasarPrinter import HasarPrinter
 
 import argparse
 import platform
---
+++ print.sh
@@ -0,0 +1,2 @@
+python PybFiscal.py -p /dev/ttyUSB0 -i entrada.txt -o aa.txt -s 9600
+
